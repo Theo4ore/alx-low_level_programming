9aeb45c3c19a74602950725649142268c1eef3ca
Theodore Nyaakpiin <nyaakpiinntheodore@gmail.com>
2023-03-17 13:39:39 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - print exat given text
+ *
+ * Return: 1
+*/
+int main(void)
+{
+	const char msg[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	write(2, msg, sizeof(msg) - 1);
+	return 1;
+}
