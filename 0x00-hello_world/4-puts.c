bb5fb468d3526997d430f7883a33bb52fad9b536
Theodore Nyaakpiin <nyaakpiinntheodore@gmail.com>
2023-03-17 11:07:34 -0700

    My first c programme with ALX

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - use puts to print a line of code
+ *
+ * Return: 0
+*/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
