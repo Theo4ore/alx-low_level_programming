958571cce577c9f17d612e1d7222428a88102ae0
Theodore Nyaakpiin <nyaakpiinntheodore@gmail.com>
2023-03-20 18:34:19 -0700

    combination for success

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - prints all possible combinations of single-digit numbers
+ *
+ * Return: 0 always as SUCCESS
+*/
+int main(void)
+{
+	int x;
+
+	for (x = 10; x < 20; x++)
+	{
+		putchar((x % 10) = '0');
+		if (x != 19)
+		{
+			putchar(',');
+			putchar('');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
