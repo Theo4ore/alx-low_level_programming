0ac14916445d54d26a8377a8fc992a6ac44dc71a
Theodore Nyaakpiin <nyaakpiinntheodore@gmail.com>
2023-03-20 18:50:48 -0700

    more brains

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - Print all possible different combinations of two digits
+ *
+ * Return: 0 as SUCCESS
+*/
+int main(void)
+{
+	int q;
+	int r;
+
+	for (q = 10; q <= 19; q++)
+	{
+		for ((r % 10) > (q % 10))
+		{
+			putchar((q % 10) + '0');
+			putchar((r % 10) + '0');
+			if (q != 18 || r != 19)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
