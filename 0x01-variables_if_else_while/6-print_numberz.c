cd41f1d4e893d195c3b20af1bd18d1e9f4441ea9
Theodore Nyaakpiin <nyaakpiinntheodore@gmail.com>
2023-03-20 17:48:58 -0700

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - prints all single digit numbers of base 10 from 0 using putchar
+ *
+ * Return : 0 as SUCCESS
+*/
+int main(void)
+{
+	int q;
+
+	for (q = 0; q < 10; q++)
+	{
+		putchar(q);
+	}
+	putchar('\n');
+return (0);
+}
