6d381826feb24a77d10889e74502d0ebc129cf6a
Theodore Nyaakpiin <nyaakpiinntheodore@gmail.com>
2023-03-28 14:19:14 -0700

    7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -8,15 +8,15 @@
 void puts_half(char *str)
 {
 	int x, y;
-	x = 0;
 
+	x = 0;
 	while (str[x] != '\0')
 	{
 		x++;
 	}
 	if (x % 2 == 1)
 	{
-		y = (x -1) / 2;
+		y = (x - 1) / 2;
 		y += 1;
 	}
 	else
