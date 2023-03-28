356b1ced5a22df2374dfe04dc2d81ac39f1d13a9
Theodore Nyaakpiin <nyaakpiinntheodore@gmail.com>
2023-03-28 13:26:51 -0700

    5-rev_string.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * rev_string - Reverse rev_string
+ * @s: String to be reversed
+ *
+ * Return: Void
+*/
+void rev_string(char *s)
+{
+	int length = 0, index = 0;
+	char temp;
+
+	while (s[index++])
+		length++;
+	for (index = length - 1; index >= length / 2; index--)
+	{
+		temp = s[index];
+		s[index] = s[length - index - 1];
+		s[length - index - 1] = temp;
+	}
+}
