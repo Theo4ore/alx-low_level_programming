7e5b5a34cb05bdbae1d2fe4ff5f9a5082906e3be
Theodore Nyaakpiin <nyaakpiinntheodore@gmail.com>
2023-03-28 16:31:16 -0700

    0-main.c

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * main - test for a postitive or negative
+ *
+ * Return: 0 when SUCCESSFUL
+ */
+int main(void)
+{
+	int x;
+
+	x = 0;
+	postitive_or_negative(x);
+	return (0);
+}
